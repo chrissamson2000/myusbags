1d15cc583a318b064bab2e294a562d4ff2e0f0e1
Christopher Samson <Chrissamson2000@gmail.com>
2024-12-06 00:03:20 -0500

    Add React Router and basic pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home'; // Import Home component
+import About from './About'; // Import About component
+import Navigation from './Navigation'; // Import Navigation component
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Links */}
+        <Navigation />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+// src/Home.js
+function Home() {
+    return <h1>Welcome to the Home Page!</h1>;
+  }
+  
+  export default Home;
+  
